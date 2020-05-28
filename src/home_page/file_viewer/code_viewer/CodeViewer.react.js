027b5c2cb192e3c3c964f9b3bcf40402b87d037d
diegocardozo97 <diego_a_cardozo@hotmail.com>
2020-05-28 11:41:32 -0500

    Add CodeMirror to be the CodeViewer @pcruiher08
    This was done with help of Pablo C. Ruiz @pcruiher08.

--- src/home_page/file_viewer/code_viewer/CodeViewer.react.js
+++ src/home_page/file_viewer/code_viewer/CodeViewer.react.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 
+import {Controlled as RCodeMirror} from 'react-codemirror2';
+
 import {validateAndRequestCodeWithParams} from 'home_page/file_viewer/code_viewer/CodeRequester.js';
+
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
+import 'home_page/file_viewer/code_viewer/CodeMirrorCustom.css';
 
 class RCodeViewer extends Component {
   /** 
@@ -22,7 +28,20 @@ class RCodeViewer extends Component {
   render() {
     return (
       <div>
-        <pre>{this.state.code}</pre>
+        <RCodeMirror
+          value={this.state.code}
+          options={{
+            mode: null,
+            lineNumbers: true,
+            // Enables browser text search and helps to set the height
+            // of the editor automatically.
+            viewportMargin: Infinity,
+            readOnly: true,
+          }}
+          onBeforeChange={(editor, data, value) => {
+            this.setState({code: value});
+          }}
+        />
       </div>
     );
   }
