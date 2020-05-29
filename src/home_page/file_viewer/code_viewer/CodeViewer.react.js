c0d56010247fa13858d87d306905622b32b7ec91
diegocardozo97 <diego_a_cardozo@hotmail.com>
2020-05-28 20:51:23 -0500

    Add to change mode by ext in CodeMirror

--- src/home_page/file_viewer/code_viewer/CodeViewer.react.js
+++ src/home_page/file_viewer/code_viewer/CodeViewer.react.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 
+// TODO: Fix the use of addons and instead of
+// this and choose here what mode, use loadmode.js.
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/clike/clike';
+import 'codemirror/mode/python/python';
 import {Controlled as RCodeMirror} from 'react-codemirror2';
 
 import {validateAndRequestCodeWithParams} from 'home_page/file_viewer/code_viewer/CodeRequester.js';
@@ -7,6 +12,7 @@ import {validateAndRequestCodeWithParams} from 'home_page/file_viewer/code_viewe
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/material.css';
 import 'home_page/file_viewer/code_viewer/CodeMirrorCustom.css';
+
 
 class RCodeViewer extends Component {
   /** 
@@ -26,24 +32,53 @@ class RCodeViewer extends Component {
   }
 
   render() {
+    const CMOptions = {
+      // This is set when files is get.
+      mode: this.getModeNameByExtension(),
+      lineNumbers: true,
+      // Enables browser text search and helps to set the height
+      // of the editor automatically.
+      viewportMargin: Infinity,
+      readOnly: true,
+    };
+
     return (
       <div>
         <RCodeMirror
           value={this.state.code}
-          options={{
-            mode: null,
-            lineNumbers: true,
-            // Enables browser text search and helps to set the height
-            // of the editor automatically.
-            viewportMargin: Infinity,
-            readOnly: true,
-          }}
+          options={CMOptions}
           onBeforeChange={(editor, data, value) => {
             this.setState({code: value});
           }}
         />
       </div>
     );
+  }
+
+  getModeNameByExtension() {
+    if (!this.props.defaultSearchParams.has('file')) {
+      return null;
+    }
+    const filePath = this.props.defaultSearchParams.get('file');
+
+    const extension = /.+\.([^.]+)$/.exec(filePath);
+    if (!extension) {
+      return null;
+    }
+
+    switch(extension[1]) {
+      case 'js':
+        return 'javascript';
+      case 'c':
+      case 'h':
+      case 'cpp':
+      case 'ino':
+        return 'clike';
+      case 'py':
+        return 'python';
+      default:
+        return null;
+    }
   }
 }
 
